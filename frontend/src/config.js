a94d2380a0c9e285a004bfa5e0be0594e545783c
Guilherme Rios <guirios85@gmail.com>
2026-05-28 17:41:09 -0300

    atualizacao codigo

--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const apiFromQuery = params.get("api");
-
-export const API_BASE_URL = apiFromQuery || localStorage.getItem("apiBaseUrl") || "http://localhost:8080";
-export const REQUEST_TIMEOUT_MS = 10000;
